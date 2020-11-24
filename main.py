5b7efa79eb310462beb0dbd781bec69d6943eeb2
Satish Kumar <satish.kumar@kasasa.com>
2020-11-24 15:23:18 -0600

    Updates to allow the ability to provide the QUEUE_NAME through an environment variable

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import boto3
+import os
+
 
 def run():
-  print('Watching for messages')
-  sqs = boto3.resource('sqs')
-  queue = sqs.get_queue_by_name(QueueName='keda-test')
+    print('Watching for messages')
+    sqs = boto3.resource('sqs')
+    queue_name = os.environ.get("QUEUE_NAME", "keda-test")
+    queue = sqs.get_queue_by_name(QueueName=queue_name)
 
-  while True:
-    for message in queue.receive_messages(WaitTimeSeconds=20):
-      print(f"Message from queue {message.body}")
-      message.delete()
+    while True:
+        for message in queue.receive_messages(WaitTimeSeconds=20):
+            print(f"Message from queue {message.body}")
+            message.delete()
+
 
 if __name__ == '__main__':
-  run()
+    run()
